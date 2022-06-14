8ff63679d177278be8e3a403cc0f3895c1fa5429
a-bakos <bakosattila@fastem.com>
2022-06-14 18:56:31 +0100

    Add insensitive search test

--- src/lib.rs
+++ src/lib.rs
@@ -42,14 +42,30 @@ mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
 
@@ -76,6 +92,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut matches: Vec<&str> = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
+            matches.push(line);
+        }
+    }
+    matches
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches: Vec<&str> = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             matches.push(line);
         }
     }
