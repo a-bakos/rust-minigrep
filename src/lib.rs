3b7703a1dd67c4d9bf0b3ed27df04b9cbe8c6f50
a-bakos <bakosattila@fastem.com>
2022-06-14 06:38:44 +0100

    Splitting the code into a library crate (WIP)

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use std::error::Error;
+use std::fs;
+
+// Box<dyn Error> => trait object
+// it means the function will return a type that implements the
+// Error trait, but we don't have to specify what particular
+// type the return value will be. This gives us flexibility to
+// return error values that may be of different types in
+// different error cases.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+impl Config {
+    // &Vec<String> = &[String]
+    pub fn new(args: &Vec<String>) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        // Easiest though somewhat inefficient way, .clone()
+        Ok(Self {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
@@ -17,38 +15,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-// Box<dyn Error> => trait object
-// it means the function will return a type that implements the
-// Error trait, but we don't have to specify what particular
-// type the return value will be. This gives us flexibility to
-// return error values that may be of different types in
-// different error cases.
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    // &Vec<String> = &[String]
-    fn new(args: &Vec<String>) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-
-        // Easiest though somewhat inefficient way, .clone()
-        Ok(Self {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-        })
     }
 }
